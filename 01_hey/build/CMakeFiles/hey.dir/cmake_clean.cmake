e6646cd659f2c02dad5c9a9ab787586e0645275c
isaac <isaachook@gmail.com>
2017-07-18 10:30:21 +1000

    initial commit

---
+++ 01_hey/build/CMakeFiles/hey.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hey.dir/hey.cpp.o"
+  "hey.pdb"
+  "hey"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hey.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 02_student/include/Student.h
@@ -0,0 +1,9 @@
+#include <string>
+
+class Student{
+    private: std::string name;
+    public: 
+        Student();
+        Student(std::string n);
+        void whois();
+};
