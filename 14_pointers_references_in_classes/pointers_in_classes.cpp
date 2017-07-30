75614b4cc384aa9124bb9b5bf00fe1f42bf7dd87
isaac <isaachook@gmail.com>
2017-07-30 18:48:56 +1000

    most recent progress

--- 14_pointers_references_in_classes/pointers_in_classes.cpp
+++ 14_pointers_references_in_classes/pointers_in_classes.cpp
@@ -15,18 +15,18 @@ int main(){
     Base b;
     Derived d;
 
-    cout << b.getName() << endl;
-    cout << d.getName() << endl;
+    cout << b.getName() << endl; // Base
+    cout << d.getName() << endl; // Derived
 
     Derived &rd = d;
-    cout << rd.getName() << endl;
+    cout << rd.getName() << endl; // Derived
 
     Derived *pd = &d;
-    cout << pd->getName() << endl;
+    cout << pd->getName() << endl; // Derived
 
     Base &rb = d;
-    cout << rb.getName() << endl;
+    cout << rb.getName() << endl; // Base
 
     Base *pb = &d;
-    cout << pb->getName() << endl;
+    cout << pb->getName() << endl; // Base
 }
